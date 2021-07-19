8ac6faf4f9d5a57ae40ee7b0242e78e334a4948a
김민지 <swiji1004@innerwave.co.kr>
2021-07-19 10:30:18 +0900

    domain service 주석 추가

--- src/main/java/com/surfinn/glzza/service/DomainService.java
+++ src/main/java/com/surfinn/glzza/service/DomainService.java
@@ -20,14 +20,16 @@ public class DomainService {
 	
 	// 도메인 목록 조회 (검색)
 	public BaseVO selectDomainList(DomainVO domainVO, BaseVO baseVO) {
-		
+		// 컬럼 정렬
 		if(!CommonUtil.isEmpty(domainVO.getColumns())) {
 			domainVO.setSorting(domainVO.getColumns()[domainVO.getISortCol_0()]);
 		}
-		
+		// domainVO와 baseVO를 매개변수로 검색한 단어와 일치하는 단어 검색
 		List<DomainVO> list = domainDao.selectDomainList(domainVO, baseVO);
 		
+		// 검색 결과가 있다면
 		if(list.size() > 0) {
+			// DataLen, DcmlLen, DomainDscrpt 중 빈 값에 - 를 넣어준다
 			for(int i = 0; i < list.size(); i++) {
 				if(StringUtils.isEmpty(list.get(i).getDataLen())) {
 					list.get(i).setDataLen("-");
@@ -46,8 +48,11 @@ public class DomainService {
 			}
 		}
 		
+		// 총 결과 갯수를 표시해줌
 		baseVO.setRecordsTotal(domainDao.selectTotalCountDomain(domainVO));				
+		// 페이징 처리에 따라 필터링된 갯수를 표시해줌
 		baseVO.setRecordsFiltered(list.size());
+		// baseVO에 Data attribute에 불러온 list 전달
 		baseVO.setData(list);
 		return baseVO;
 	}
@@ -58,9 +63,12 @@ public class DomainService {
 	}
 
 	// 수정, 저장 기능
-	public int insertDomain(DomainVO domainVO) {		
+	public int insertDomain(DomainVO domainVO) {	
+		// 등록한사람, 수정한사람을 ADMIN으로 설정
 		domainVO.setRegId(CommonConst.REG_ID);
 		domainVO.setUpdId(CommonConst.UPD_ID);
+		
+		// DomainDscrpt가 있다면 좌우공백 제거 및 \r\n을 <br>태그로 변경해준다
 		if(!StringUtils.isEmpty(domainVO.getDomainDscrpt())) {
 			domainVO.setDomainDscrpt(domainVO.getDomainDscrpt().trim().replace("\r\n","<br>"));
 		}
@@ -69,6 +77,8 @@ public class DomainService {
 		
 		DomainVO checkData = domainDao.selectDomain(domainVO);
 		
+		// DB에 USEYN = N으로 되어있는 데이터가 있다면 해당 데이터를 수정하고,
+		// 기존에 DB에 없었다면 데이터를 생성함
 		if(!CommonUtil.isEmpty(checkData)) {
 			if(checkData.getUseYn().equals("N")) {
 				result = domainDao.updateUseYn(domainVO);
@@ -99,6 +109,7 @@ public class DomainService {
 		int result = 0;
 		DomainVO checkData = domainDao.selectDomainDomainName(domainVO);
 		
+		// 도메인 명이 중복이면 result = 1 을 반환함
 		if(!CommonUtil.isEmpty(checkData)) {					
 			if(!checkData.getDomainSeq().equals(domainVO.getDomainSeq())) {
 				if(checkData.getDomainNm().equals(domainVO.getDomainNm())) {
